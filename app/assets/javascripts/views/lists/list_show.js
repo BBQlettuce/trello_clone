5fee98455b8fa6bb7328a28cafb7a3845725090e
oscarliu <oscar.liu.13q@gmail.com>
2015-09-14 12:37:24 -0400

    add subview methods for lists/cards

--- app/assets/javascripts/views/lists/list_show.js
+++ app/assets/javascripts/views/lists/list_show.js
@@ -2,27 +2,35 @@ TrelloClone.Views.ListShow = Backbone.CompositeView.extend({
   template: JST['lists/show'],
 
   className: 'list',
-  
+
   initialize: function() {
     // debugger
     this.collection = this.model.cards();
     this.listenTo(this.model, "sync", this.render);
     this.listenTo(this.collection, "add", this.addCardView);
+    this.listenTo(this.collection, "remove", this.removeListView);
     this.collection.each(this.addCardView.bind(this));
   },
 
-  // events: {
-  //
-  // },
+  events: {
+    "click .card-adder": "renderCardAdder",
+    "click .delete-list": "delete-list"
+  },
 
   addCardView: function(card) {
     var subview = new TrelloClone.Views.CardShow({model: card});
     this.addSubview('.card-list', subview);
   },
 
+  removeCardView: function(card) {
+    this.removeModelSubview('.card-list', list);
+  },
+
   render: function() {
     this.$el.html(this.template({list: this.model}))
     this.attachSubviews();
     return this;
-  }
+  },
+
+
 })
